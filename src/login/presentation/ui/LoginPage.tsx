458b95ba1c04ecf3ff58e001df9a79dca388860f
rem <remi.fernandez@vinci-energies.net>
2025-09-04 08:41:10 +0900

    feat(login): icon size

--- src/login/presentation/ui/LoginPage.tsx
+++ src/login/presentation/ui/LoginPage.tsx
@@ -5,8 +5,8 @@ import LogoScylla from "@/assets/logo_scylla.png"
 export const LoginPage = () => {
     return (
         <div className={"flex items-center h-screen flex-col"}>
-            <div className="flex flex-row items-center space-x-1 mb-4 mt-6 pr-6">
-                <img src={LogoScylla} alt="logo" className="object-contain w-48 h-48" />
+            <div className="flex flex-row items-center space-x-1 mb-4 mt-12 pr-6">
+                <img src={LogoScylla} alt="logo" className="object-contain w-32 h-32" />
                 <h1 className="text-4xl font-bold">Scylla</h1>
             </div>
             <Card className="w-full max-w-sm">
