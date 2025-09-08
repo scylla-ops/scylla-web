d52d3d4554af635d93a1299a7a53474dfd510fde
rem <remi.fernandez@vinci-energies.net>
2025-09-08 16:26:59 +0900

    chore: init lingui

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import lingui from "@lingui/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+      react({
+          plugins: [["@lingui/swc-plugin", {}]]
+      }),
+      lingui(),
+      tailwindcss(),
+      tsconfigPaths()],
 })
