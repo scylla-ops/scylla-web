55717aade13c08d3d653ecd450583a7a3c8d3101
rem <remi.fernandez@vinci-energies.net>
2025-10-27 16:50:37 +0900

    feat(frontend/marketplace): research bar

--- src/modules/marketplace/presentation/ui/MarketItemList.tsx
+++ src/modules/marketplace/presentation/ui/MarketItemList.tsx
@@ -9,20 +9,24 @@ export type MarketItem = {
 
 export type MarketItemListProps = {
   items: MarketItem[];
+  filter: string;
 };
 
-export const MarketItemList = ({ items }: MarketItemListProps) => {
+export const MarketItemList = ({ items, filter }: MarketItemListProps) => {
   return (
     <div className={'flex flex-row flex-wrap h-fit gap-4'}>
-      {items.map((item, index) => (
-        <MarketItemCard
-          key={index}
-          className='flex-1 min-w-[300px] max-w-[400px] h-50'
-          provider={item.provider}
-          title={item.title}
-          descrption={item.descrption}
-        />
-      ))}
+      {items.map((item, index) => {
+        if (item.title.includes(filter))
+          return (
+            <MarketItemCard
+              key={index}
+              className='flex-1 min-w-[300px] max-w-[400px] h-50'
+              provider={item.provider}
+              title={item.title}
+              descrption={item.descrption}
+            />
+          );
+      })}
     </div>
   );
 };
--- src/modules/marketplace/presentation/ui/MarketplacePage.tsx
+++ src/modules/marketplace/presentation/ui/MarketplacePage.tsx
@@ -1,12 +1,13 @@
-import MarketItemCard, {
-  type MarketItemCardProps,
-} from '@/modules/marketplace/presentation/ui/MarketItemCard.tsx';
 import {
   type MarketItem,
   MarketItemList,
 } from '@/modules/marketplace/presentation/ui/MarketItemList.tsx';
+import { Input } from '@/modules/core/presentation/ui/shadcn';
+import { useState } from 'react';
 
+//TODO: for the topbar, make it reusable for each module of the app
 export const MarketplacePage = () => {
+  const [filter, setFilter] = useState('');
   const items: MarketItem[] = [
     { provider: 'Corp', title: 'Title', descrption: 'Description' },
     { provider: 'Corp', title: 'Title', descrption: 'Description' },
@@ -24,8 +25,20 @@ export const MarketplacePage = () => {
   ];
 
   return (
-    <div className={'flex bg-background h-screen p-2'}>
-      <MarketItemList items={items} />
+    <div className='flex flex-col bg-background h-screen'>
+      <div className='flex flex-row w-full justify-between bg-gray-100 border-b-2 items-center p-2'>
+        <Input
+          value={filter}
+          onChange={e => setFilter(e.target.value)}
+          placeholder='Search'
+          className='w-[20%] bg-background'
+        />
+        <p>Account: Example account</p>
+        <p>Connected in: Example organisation</p>
+      </div>
+      <div className='flex flex-col gap-4 w-full h-full p-2'>
+        <MarketItemList items={items} filter={filter} />
+      </div>
     </div>
   );
 };
