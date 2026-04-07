fa02eeb55e23c1cb41de6a316e687932970bdfad
rem <remi.fernandez@vinci-energies.net>
2026-04-07 13:58:26 +0900

    refactor(frontend): custom sidebar trigger

---
+++ src/modules/layout/presentation/ui/ScyllaSidebarTrigger.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Button } from '@shadcn';
+import { cn } from '@shared/presentation/utils';
+import { ArrowLeft } from 'lucide-react';
+import { useSidebar } from '@shadcn/sidebar.tsx';
+
+//Custom from shadcn for scylla
+export function ScyllaSidebarTrigger({
+  className,
+  onClick,
+  ...props
+}: React.ComponentProps<typeof Button>) {
+  const { toggleSidebar, open } = useSidebar();
+
+  return (
+    <Button
+      data-sidebar='trigger'
+      data-slot='sidebar-trigger'
+      variant='ghost'
+      size='icon'
+      className={cn('size-7', className)}
+      onClick={event => {
+        onClick?.(event);
+        toggleSidebar();
+      }}
+      {...props}
+    >
+      <ArrowLeft
+        className={cn('size-4 transition-transform duration-300', !open && 'rotate-180')}
+      />
+      <span className='sr-only'>Toggle Sidebar</span>
+    </Button>
+  );
+}
+
+export default ScyllaSidebarTrigger;
--- src/modules/layout/presentation/ui/TopBar.tsx
+++ src/modules/layout/presentation/ui/TopBar.tsx
@@ -1,11 +1,10 @@
-import { SidebarTrigger } from '@/modules/shared/presentation/ui/shadcn/sidebar.tsx';
 import { ScyllaBreadcrumbs } from '@/modules/layout/presentation/ui/ScyllaBreadcrumbs.tsx';
+import ScyllaSidebarTrigger from '@/modules/layout/presentation/ui/ScyllaSidebarTrigger.tsx';
 
-//TODO: variable for the bg of the topbar
 export const TopBar = () => {
   return (
     <header className={'flex flex-row p-2 items-center  gap-4'}>
-      <SidebarTrigger />
+      <ScyllaSidebarTrigger />
       <ScyllaBreadcrumbs />
     </header>
   );
